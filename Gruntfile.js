d3fb55335275ede4834e07d2c5be1e7ad4bb630d
jbranchaud <jbranchaud@gmail.com>
2014-01-28 10:49:07 -0300

    Adding tasks to preprocess assets, put them in the assets dir, and then either build, serve, or deploy jekyll.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+        less: {
+            production: {
+                options: {
+                    compress: true,
+                    yuicompress: true,
+                    paths: ['bower_components/bootstrap/less']
+                },
+                files: {
+                    'assets/css/bootstrap.min.css': 'bower_components/bootstrap/less/bootstrap.less'
+                }
+            }
+        },
+        uglify: {
+            jquery: {
+                files: {
+                    'assets/js/jquery.min.js': 'bower_components/jquery/jquery.js'
+                }
+            }
+        },
+        copy: {
+            normalize: {
+                files: [
+                    {expand: true, cwd: 'bower_components/normalize-css/', src: ['normalize.css'], dest: 'assets/css/'}
+                ]
+            },
+            fontawesome: {
+                files: [
+                    {expand: true, cwd: 'bower_components/font-awesome/css/', src: ['font-awesome.min.css'], dest: 'assets/css/'}
+                ]
+            }
+        },
+        exec: {
+            build: {
+                cmd: 'jekyll build'
+            },
+            serve: {
+                cmd: 'jekyll serve --watch'
+            },
+            deploy: {
+                cmd: 'git push deploy master; git push origin master;'
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-exec');
+
+    grunt.registerTask('default', [ 'less', 'uglify', 'copy', 'exec:build' ]);
+    grunt.registerTask('serve', [ 'less', 'uglify', 'copy', 'exec:serve' ]);
+    grunt.registerTask('deploy', [ 'exec:deploy' ]);
+
+};
