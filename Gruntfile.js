3c6b1215771f001c2f218e5c77963e570befc2df
jbranchaud <jbranchaud@gmail.com>
2014-02-03 16:19:17 -0300

    Add some new Grunt plugins (watch and concurrent) so that I can watch and serve this project concurrently.

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,6 +44,19 @@ module.exports = function(grunt) {
                 ]
             }
         },
+        watch: {
+            styles: {
+                files: 'css/**/*',
+                tasks: ['copy:styles']
+            },
+            images: {
+                files: 'img/**/*',
+                tasks: ['copy:images']
+            }
+        },
+        concurrent: {
+            dev: ['watch', 'exec:serve']
+        },
         exec: {
             build: {
                 cmd: 'jekyll build'
@@ -61,10 +74,12 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concurrent');
     grunt.loadNpmTasks('grunt-exec');
 
     grunt.registerTask('default', [ 'less', 'uglify', 'copy', 'exec:build' ]);
-    grunt.registerTask('serve', [ 'less', 'uglify', 'copy', 'exec:serve' ]);
+    grunt.registerTask('serve', [ 'less', 'uglify', 'copy', 'concurrent:dev' ]);
     grunt.registerTask('deploy', [ 'exec:deploy' ]);
 
 };
