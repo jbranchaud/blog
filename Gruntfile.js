8d82d49417c67ea184dab216bd832260704b261e
jbranchaud <jbranchaud@gmail.com>
2014-02-13 08:22:06 -0300

    Removing copy tasks for my CSS and the img folder from the grunt file. These files will be edited where they already belong in the assets folder.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,16 +23,6 @@ module.exports = function(grunt) {
             }
         },
         copy: {
-            images: {
-                files: [
-                    {expand: true, cwd: 'img/', src: ['**'], dest: 'assets/img/'}
-                ]
-            },
-            styles: {
-                files: [
-                    {expand: true, cwd: 'css/', src: ['styles.css', 'main.css'], dest: 'assets/css/'}
-                ]
-            },
             normalize: {
                 files: [
                     {expand: true, cwd: 'bower_components/normalize-css/', src: ['normalize.css'], dest: 'assets/css/'}
